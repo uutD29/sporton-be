09618e1491607a37b3e6f4c2adf69c57cb296b1f
uhud <uhud76@gmail.com>
2026-02-11 21:36:40 +0700

    Add categoryId mapping to create and update product controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -12,6 +12,11 @@ export const createProduct = async (
       productData.imageUrl = req.file.path;
     }
 
+    if (productData.categoryId) {
+      productData.category = productData.categoryId;
+      delete productData.categoryId;
+    }
+
     const product = new Product(productData);
     await product.save();
     res.status(201).json(product);
